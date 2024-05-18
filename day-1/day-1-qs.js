2f45606c13f086f88e201e8140a03c61d3d5408f
saloskilo <salmanafridi945@gmail.com>
2024-05-18 18:17:20 +0500

    day-1 code

---
+++ day-1/day-1-qs.js
@@ -0,0 +1,15 @@
+// Q1 - Install Node.js,TypeScript and VS Code on your computer.
+
+// Q2 - Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Salman, would you like to learn some Javascript today?”
+
+var person="salman";
+
+console.log(`Hello ${person}  would you like to learn some Javascript today? `);
+
+
+// Q3 - Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var person2="Salman Ali";
+console.log(person2.toLowerCase());
+console.log(person2.toUpperCase());
+console.log(person2.charAt(0).toUpperCase()+person2.slice(1).toLowerCase());
+
