4b39bff39031845236cbbc516025c6a59fe5c823
saloskilo <salmanafridi945@gmail.com>
2024-05-19 02:12:04 +0500

    day-3-Questions-Done

---
+++ day-3/day-3-qs.js
@@ -0,0 +1,19 @@
+
+// **Q7** - Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log(4+4);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+// **Q8** - You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+
+// **Q9** - Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var myFauriteNumber=7;
+var msg=''.concat("my Faurite number is:",myFauriteNumber);
+console.log(msg);
