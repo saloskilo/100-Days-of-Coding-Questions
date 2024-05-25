376cbb2c35f1b0c6c6c24f526c7a668297d3a863
saloskilo <salmanafridi945@gmail.com>
2024-05-25 18:02:56 +0500

    Day-19-Questions-Done

--- Day-11-to-20/day-19/day-19-qs.js
+++ Day-11-to-20/day-19/day-19-qs.js
@@ -12,3 +12,13 @@ var onlyStrings = differentTypes.filter((onlyStr) => {
 console.log(onlyStrings);
 
 // **Q57** - Find the Average Grade: Given a list of grades, calculate the average grade.
+// A list of grades
+var grades = [88, 94, 72, 99, 53, 77];
+// Calculates the average grade
+var averageGrade =
+  grades.reduce((total, grade) => {
+    return total + grade;
+  }, 0) / grades.length;
+// Shows the average grade
+console.log(averageGrade);
+// First, we add up all the grades. Then, we divide by how many grades there are to get the average.
